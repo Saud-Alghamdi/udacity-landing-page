f35ad4d372694f0cdf8fa5e6179ffd6cc9d6f299
Saud Alghamdi <saudalghamdi1997@gmail.com>
2022-10-12 11:41:32 +0300

    Project finished, modify READMDE.md remaining

--- js/app.js
+++ js/app.js
@@ -23,20 +23,66 @@
  * 
 */
 
-/**
-//--APPROACH--//
-     - The section within the browser view is gonna get its header red INITIALLY ONLY.
-     - It's gotta be dynamic because I don't know how many section we are going to have.
-     - I will grab every section with querySelectorAll(), and onscroll I will run a function that constantly checks which section is within and view and which is not.
-     - The ones in the view I add to them the active class, the ones not i will remove from them the active class, by default all sections have no active class.
-**/
-
+const html = document.querySelector('html');
 const sections = Array.from(document.querySelectorAll('section'));
 const viewportHeight = window.innerHeight;
+const navbar = document.querySelector('.navbar__menu');
+const navbarList = document.querySelector('#navbar__list');
+const numberOfNavItems = Array.from(document.querySelectorAll('[data-nav]')).length;
+const scrollTopPageIcon = document.querySelector('.fa-angles-up');
 
-window.onscroll = () => {
+/**
+ * End Global Variables
+**/
+
+
+// Build the nav
+for(let i = 0; i < numberOfNavItems; i++) {
+
+    const navbarItem = document.createElement('li');
+    const navbarLink = document.createElement('a');
+
+    navbarLink.classList.add("menu__link");
+    navbarLink.innerText = "Section " + (i + 1);
+
+    // Creates a dataset for each nav item, the dataset holds the section the nav item refers to, to bind that dataset with the section ID for navigation.
+    const sectionAssociated = "section" + (i + 1);
+    navbarLink.dataset.association = sectionAssociated;
+
+    navbarLink.addEventListener('click', e => {
+        document.getElementById(navbarLink.dataset.association).scrollIntoView({behavior: 'smooth'})
+    });
     
-    // Checks which section is within the viewport and adds "active" class to it.
+    navbarList.appendChild(navbarItem);
+    navbarItem.appendChild(navbarLink);
+}
+
+
+// Scroll inactivity
+let hideNavbar = setTimeout(() => {
+    navbar.style.display = 'none';
+}, 3000);
+
+
+//----- Window Scrolling Logics -----//
+window.onscroll = () => {
+
+    clearTimeout(hideNavbar);
+    navbar.style.display ='block';
+    
+    // Back to top icon
+    if(html.getBoundingClientRect().top !== 0) {
+        scrollTopPageIcon.classList.add('active');
+    } else {
+        scrollTopPageIcon.classList.remove('active');
+    }
+
+    scrollTopPageIcon.addEventListener('click', e => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+    
+    
+    // Checks which section is within the viewport then adds "active" class to it.
     sections.forEach(section => {
 
         let sectionSpaceFromTop = section.getBoundingClientRect().top;
@@ -47,36 +93,13 @@ window.onscroll = () => {
             section.classList.remove("active");
         }
     });
+
+    // Scroll inactivity
+    hideNavbar = setTimeout(() => {
+        navbar.style.display = 'none';
+    }, 3000);
+    
 };
-
-const navList = document.querySelector('.nav__list');
-const section1 = document.querySelector('#section1');
-const section2 = document.querySelector('#section2');
-const section3 = document.querySelector('#section3');
-
-// <li class="navbar__item"><a class="navbar__link">Hello</a></li>
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
 
 
 /**
